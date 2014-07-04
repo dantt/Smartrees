0bd4992d515c95dc271051ffb31e79d30ae17af1
giulio <giuliolovisotto@gmail.com>
2014-07-04 18:21:26 +0200

    Refactored controller, now the control is only in ia.js. Not really tested this though c:

--- ia.js
+++ ia.js
@@ -5,11 +5,13 @@
 /**********************************/
 
 function IA(tree){
-  this.problem = new Problem(tree);
-  this.problem.strategy = algoMap[iaSettings.getOption('defaultStrategy')];
-  this.control = new Controller(this.problem);
-  this.treemanager = new TreeManager(this.problem.getTree.bind(this.problem));
-  this.settings = iaSettings;
+    this.problem = new Problem(tree);
+    this.problem.strategy = algoMap[iaSettings.getOption('defaultStrategy')];
+    //this.control = new Controller(this.problem);
+    this.treemanager = new TreeManager(this.problem.getTree.bind(this.problem));
+    this.settings = iaSettings;
+    this._isPlaying = false;
+
 }
 
 
@@ -23,12 +25,41 @@ function IA(tree){
 /*********************************/
 
 IA.prototype.problem;
-IA.prototype.control;
+//IA.prototype.control;
 IA.prototype.treemanager;
+IA.prototype._intervalId;
+IA._isPlaying = false;
+
+IA.prototype.step = function(){
+    this.problem.step();
+};
 
 
 
+//HERE BE DRAGONS.
+//THIS WILL BE REFACTORED W/SIGNALS & TRIGGERS
+IA.prototype.play = function(){
+    debug("method play" + this._isPlaying);
+    if (this._isPlaying == false){
+      this._isPlaying = true;
+      $('#img_play').attr("src", "images/play_h.png");
+      $('#img_stop').attr("src", "images/pause.png");
+      this.step();
+      this._intervalId = setInterval(this.step.bind(this), iaSettings.getOption('simulationSpeed'));
 
+    }
+};
+
+
+
+IA.prototype.stop = function(){
+    if (this._isPlaying == true){
+      clearInterval(this._intervalId);
+      this._isPlaying = false;
+      $('#img_play').attr("src", "images/play.png");
+      $('#img_stop').attr("src", "images/pause_h.png");
+    }
+};
 
 /*********************************/
 /*********************************/
@@ -37,21 +68,21 @@ IA.prototype.treemanager;
 /*********************************/
 
 IA.prototype.setStrategy = function(strategy) {
-	this.problem.strategy = algoMap[strategy];
+    this.problem.strategy = algoMap[strategy];
 };
 
 IA.prototype.setOrder = function(order) {
-	this.problem.setOrder(order);
-	//debug(order);
+    this.problem.setOrder(order);
+    //debug(order);
 };
 
 
 IA.prototype.setTree = function(tree) {
-	this.problem.setTree(tree);
+    this.problem.setTree(tree);
 }
 
 IA.prototype.setOptions = function(options) {
-	this.settings.setOptions(options);
+    this.settings.setOptions(options);
 }
 
 
@@ -64,34 +95,23 @@ IA.prototype.setOptions = function(options) {
 /*****************************/
 
 IA.prototype.newTree = function(tree) {
-	this.problem.setTree(tree);
-	this.treemanager = new TreeManager(this.problem.getTree.bind(this.problem));
+    this.problem.setTree(tree);
+    this.treemanager = new TreeManager(this.problem.getTree.bind(this.problem));
 }
 
 IA.prototype.reset = function() {/*
-	this.problem.frontier = [];
-	this.problem._tree = this.problem._startingTree;*/
+ this.problem.frontier = [];
+ this.problem._tree = this.problem._startingTree;*/
     this.newTree(this.problem._startingTree);
-	this.draw('treecontainer');
-	$('#output').html("");
+    this.draw('treecontainer');
+    $('#output').html("");
 };
 
 IA.prototype.draw = function(container) {
-	this.treemanager.draw(container);
+    this.treemanager.draw(container);
 };
 
 IA.prototype.update = function() {
-	this.treemanager.update();
+    this.treemanager.update();
 };
 
-IA.prototype.step = function() {
-	this.control.step();
-};
-
-IA.prototype.play = function() {
-	this.control.play();
-};
-
-IA.prototype.stop = function() {
-	this.control.stop();
-};
